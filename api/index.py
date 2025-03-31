d626a55c7b42d8c313803791192100e0857d794a
Girish Vishveshvara Bhat <159102618+GirishIITM@users.noreply.github.com>
2025-03-31 18:59:48 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
+BASE_URL = "http://74.176.192.16:8000"
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/', defaults={'path': ''}, methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
+@app.route('/<path:path>', methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
+def proxy(path):
+    url = f"{BASE_URL}/{path}"
+    response = requests.request(
+        method=request.method,
+        url=url,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False
+    )
+    return (response.content, response.status_code, response.headers.items())
 
-@app.route('/about')
-def about():
-    return 'About'
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
